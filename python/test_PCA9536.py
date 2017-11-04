b5aa97f06fc1ae92a125f2f12b94cb01775abdca
Mark Grimes <mark.grimes@rymapt.com>
2017-11-04 12:41:44 +0000

    Add execute permissions to the file, and remove some excess whitespace

--- python/test_PCA9536.py
+++ python/test_PCA9536.py
@@ -19,37 +19,37 @@ class TestPCA9536(unittest.TestCase):
 
     def setUp(self):
         self.device = PCA9536.PCA9536( smbus.SMBus(1) )
-        
+
     def test_polarityReg(self):
 		#device = PCA9536.PCA9536( smbus.SMBus(1) )
 		testValue= 0xAA #10101010
 		self.device.setInvertReg(testValue)
 		res= self.device.getInvertReg()
 		self.assertEqual(res, testValue)
-		
+
 		testValu= 0x55 #01010101
 		self.device.setInvertReg(testValue)
 		res= self.device.getInvertReg()
 		self.assertEqual(res, testValue)
-		
+
     def test_directionReg(self):
 		testValue= 0xAA #10101010
 		self.device.setIOReg(testValue)
 		res= self.device.getIOReg()
 		self.assertEqual(res, testValue)
-		
+
 		testValue= 0x55 #01010101
 		self.device.setIOReg(testValue)
 		res= self.device.getIOReg()
 		self.assertEqual(res, testValue)
-		
+
     def test_writeReadPin(self):
 		# set pins as outputs
 		self.device.setIOReg(0x00)
 		# set no-inversion
 		self.device.setInvertReg(0x00)
 		# set outputs to a configuration (testValue)
-		testValue= 0x1 
+		testValue= 0x1
 		self.device.setOutputs(testValue)
 		# read the output buffer to test it
 		res= self.device.getOutputs()
