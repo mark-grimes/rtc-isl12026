77e8ada211f1766602e94a83329679b18c9a07c4
Mark Grimes <mark.grimes@rymapt.com>
2017-11-10 12:55:54 +0000

    Add ability to get and set gain of TSL2591; expand tests; fix bug where high bit wasn't shifted enough

--- python/TSL2591.py
+++ python/TSL2591.py
@@ -7,49 +7,77 @@ __author__    = "Mark Grimes"
 __copyright__ = "Copyright 2017, Rymapt Ltd"
 __license__   = "To be decided"
 
+GAIN_LOW  = 0x00  # 1x
+GAIN_MED  = 0x10  # (min=22x, typical=24.5x, max=27x) for both channels
+GAIN_HIGH = 0x20  # (min=360x, typical=400x, max=440x) for both channels
+GAIN_MAX  = 0x30  # (min=8500x, typical=9200x, max=9900x) for channel 0; (min=9100x, typical=9900x, max=10700x) for channel 1
+
 class TSL2591(object):
+
     def __init__(self, bus=None):
         if bus==None: self._bus=smbus.SMBus(1)
         else: self._bus=bus
         self._address=0x29
-        
+
         # Make sure that I can correctly read the device ID (should always be 0x50)
         if( self._readRegister(0x12)!=0x50 ):
             raise Exception("TSL2591 device does not respond with the correct device ID")
-    
+
+        config = self._readConfigRegister()
+        self._integrationTime = config & 0x07
+        self._gain = config & 0x30
+
     def powerOn(self):
-        self.setEnable( 0x02|0x01 ) # 0x02 is ALS enable; 0x01 is power on
+        self._writeEnableRegister( 0x02|0x01 ) # 0x02 is ALS enable; 0x01 is power on
 
     def powerOff(self):
-        self.setEnable( 0x00 )
+        self._writeEnableRegister( 0x00 )
 
-    def status(self):
-        return self._readRegister( 0x13 )
+    def getGain(self):
+        return self._gain
 
-    def getConfig(self):
-        return self._readRegister( 0x01 )
+    def setGain(self, gain):
+        if gain not in [GAIN_LOW, GAIN_MED, GAIN_HIGH, GAIN_MAX]:
+            raise Exception("Invalid gain value "+str(gain))
+        self._gain = gain
+        self._writeConfigRegister( self._gain | self._integrationTime )
 
-    def setConfig(self, data):
-        return self._writeRegister( 0x01, data )
+    def rawValues(self):
+        return self._readChannelRegisters()
 
-    def getEnable(self):
-        return self._readRegister( 0x00 )
+    def _readRegister(self, register):
+        self._bus.write_byte( self._address, (0xa0 | register) )
+        return self._bus.read_byte( self._address )
 
-    def setEnable(self, data):
-        return self._writeRegister( 0x00, data )
+    def _writeRegister(self, register, data):
+        self._bus.write_byte_data( self._address, (0xa0 | register), data )
 
-    def light(self):
+    def _readControlRegister(self):
+        return self._readRegister(0x01)
+
+    def _writeControlRegister(self, data):
+        self._writeRegister(0x01, data)
+
+    def _readEnableRegister(self):
+        return self._readRegister(0x00)
+
+    def _writeEnableRegister(self, data):
+        self._writeRegister(0x00, data)
+
+    def _readConfigRegister(self):
+        return self._readRegister(0x01)
+
+    def _writeConfigRegister(self, data):
+        self._writeRegister(0x01, data)
+
+    def _readStatusRegister(self):
+        return self._readRegister(0x13)
+
+    def _readChannelRegisters(self):
         # Data sheet says to read these in order because it uses shadow registers to make
         # sure the low and high bytes are from the same reading.
         chan0Low  = self._readRegister(0x14)
         chan0High = self._readRegister(0x15)
         chan1Low  = self._readRegister(0x16)
         chan1High = self._readRegister(0x17)
-        return (chan0High<<4 | chan0Low),(chan1High<<4 | chan1Low)
-        
-    def _readRegister(self, register):
-        self._bus.write_byte( self._address, (0xa0 | register) )
-        return self._bus.read_byte( self._address )
-    
-    def _writeRegister(self, register, data):
-        self._bus.write_byte_data( self._address, (0xa0 | register), data )
+        return (chan0High<<8 | chan0Low),(chan1High<<8 | chan1Low)
--- python/test_TSL2591.py
+++ python/test_TSL2591.py
@@ -9,12 +9,54 @@ __copyright__ = "Copyright 2017, Rymapt Ltd"
 __license__   = "To be decided"
 
 import unittest
+import time
 import TSL2591
 
 class TestTSL2591(unittest.TestCase):
-    def test_construct(self):
-        device=TSL2591.TSL2591()
 
+    def test_getRawValues(self):
+        """
+        Make sure we can get the raw values, and that higher gains provide higher results.
+        """
+        device = TSL2591.TSL2591()
+        device.powerOn()
+
+        results = {}
+        for gain in [TSL2591.GAIN_LOW, TSL2591.GAIN_MED, TSL2591.GAIN_HIGH, TSL2591.GAIN_MAX]:
+            device.setGain( gain )
+            # need to perform a sleep because it takes a little bit of time to transition
+            time.sleep(0.5)
+            results[gain] = device.rawValues()
+
+        # Channel 0 (visible and IR)
+        self.assertEqual( True, results[TSL2591.GAIN_LOW][0] <= results[TSL2591.GAIN_MED][0], str(results[TSL2591.GAIN_LOW][0])+" < "+str(results[TSL2591.GAIN_MED][0]) )
+        self.assertEqual( True, results[TSL2591.GAIN_MED][0] <= results[TSL2591.GAIN_HIGH][0], str(results[TSL2591.GAIN_MED][0])+" < "+str(results[TSL2591.GAIN_HIGH][0]) )
+        self.assertEqual( True, results[TSL2591.GAIN_HIGH][0] <= results[TSL2591.GAIN_MAX][0], str(results[TSL2591.GAIN_HIGH][0])+" < "+str(results[TSL2591.GAIN_MAX][0]) )
+        self.assertNotEqual( 0, results[TSL2591.GAIN_MAX][0] )
+
+        # Channel 1 (IR only)
+        self.assertEqual( True, results[TSL2591.GAIN_LOW][1] <= results[TSL2591.GAIN_MED][1], str(results[TSL2591.GAIN_LOW][1])+" < "+str(results[TSL2591.GAIN_MED][1]) )
+        self.assertEqual( True, results[TSL2591.GAIN_MED][1] <= results[TSL2591.GAIN_HIGH][1], str(results[TSL2591.GAIN_MED][1])+" < "+str(results[TSL2591.GAIN_HIGH][1]) )
+        self.assertEqual( True, results[TSL2591.GAIN_HIGH][1] <= results[TSL2591.GAIN_MAX][1], str(results[TSL2591.GAIN_HIGH][1])+" < "+str(results[TSL2591.GAIN_MAX][1]) )
+        self.assertNotEqual( 0, results[TSL2591.GAIN_MAX][0] )
+
+    def test_getAndSetGain(self):
+        device = TSL2591.TSL2591()
+        device.setGain( TSL2591.GAIN_LOW )
+        self.assertEqual( TSL2591.GAIN_LOW, device.getGain() );
+        self.assertEqual( TSL2591.GAIN_LOW, 0x30 & device._readConfigRegister() );
+
+        device.setGain( TSL2591.GAIN_MED )
+        self.assertEqual( TSL2591.GAIN_MED, device.getGain() );
+        self.assertEqual( TSL2591.GAIN_MED, 0x30 & device._readConfigRegister() );
+
+        device.setGain( TSL2591.GAIN_HIGH )
+        self.assertEqual( TSL2591.GAIN_HIGH, device.getGain() );
+        self.assertEqual( TSL2591.GAIN_HIGH, 0x30 & device._readConfigRegister() );
+
+        device.setGain( TSL2591.GAIN_MAX )
+        self.assertEqual( TSL2591.GAIN_MAX, device.getGain() );
+        self.assertEqual( TSL2591.GAIN_MAX, 0x30 & device._readConfigRegister() );
 
 if __name__ == "__main__":
     unittest.main()
